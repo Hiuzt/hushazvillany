9320e50197d66932b0f0a89d4c8bc8477ea5f34b
Hiuzt <kerner.daniel1@gmail.com>
2024-02-26 20:35:15 +0100

    server-address-chg

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,7 +10,7 @@ export class ProductService {
     constructor(private http: HttpClient) { }
 
     getAllProducts(): Observable<any> {
-        return this.http.get<any>('http://34.41.92.132:5000/api/Product/', { withCredentials: true });
+        return this.http.get<any>('http://34.41.92.132:5000/api/Product', { withCredentials: true });
     }
 
     addNewProduct(productForm: any): Observable<any> {
