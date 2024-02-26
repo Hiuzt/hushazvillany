b5052306d3b475e1fd5de24e0d24b12dd21838f6
Hiuzt <kerner.daniel1@gmail.com>
2024-02-26 18:17:14 +0100

    d

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,18 +10,18 @@ export class ProductService {
     constructor(private http: HttpClient) { }
 
     getAllProducts(): Observable<any> {
-        return this.http.get<any>('http://34.41.92.132:5000/api/Product', { withCredentials: true });
+        return this.http.get<any>('https://localhost:7228/api/Product/', { withCredentials: true });
     }
 
     addNewProduct(productForm: any): Observable<any> {
-        return this.http.post<any>('https://34.41.92.132:5000/api/Product', productForm, {withCredentials: true})
+        return this.http.post<any>('https://localhost:7228/api/Product/', productForm, {withCredentials: true})
     }
 
     deleteProduct(productID: number): Observable<any> {
-        return this.http.delete<any>(`https://34.41.92.132:5000/api/Product/${productID}`, {withCredentials: true})
+        return this.http.delete<any>(`https://localhost:7228/api/Product/${productID}`, {withCredentials: true})
     }
 
     updateProduct(productID: number, productForm: any): Observable<any> {
-        return this.http.put<any>(`https://34.41.92.132:5000/api/Product/${productID}`, productForm, {withCredentials: true})
+        return this.http.put<any>(`https://localhost:7228/api/Product/${productID}`, productForm, {withCredentials: true})
     }
 }
